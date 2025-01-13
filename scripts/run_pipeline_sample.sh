0477ed2c7970e86abf62f4fbebe2e2c0945eec57
gizemozd <pembe.ozdil@epfl.ch>
2025-01-13 15:18:59 +0100

    fix the pipeline script

--- scripts/run_pipeline_sample.sh
+++ scripts/run_pipeline_sample.sh
@@ -6,4 +6,6 @@ run_deeplabcut --txt_dir dirs.txt --pose2d
 # Filtering and triangulation
 run_anipose --txt_dir dirs.txt --filter_2d --calibrate --triangulate
 # Check the 3D pose estimation quality
-animate_3d --pose_path pose3d.h5 --export_path pose3d --video
+folder_path=$(head -1 dirs.txt)
+echo $folder_path
+animate_3d --pose_path ${folder_path}/001_step_3sec/behData/pose-3d/pose3d.h5 --export_path ${folder_path}/001_step_3sec/behData/pose-3d/pose3d_video --video
